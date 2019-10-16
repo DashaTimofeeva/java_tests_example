abded32c85cc65b48fe9bed1b0d1ebf1caa7afa8
Daria Timofeeva <ddtimofeeva@yandex-team.ru>
2019-10-16 04:26:20 +0300

    Added some functions

--- sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/MyFirstProgram.java
@@ -2,8 +2,30 @@ package ru.stqa.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
-    }
+    hello("Dasha");
+    hello("world");
+    hello("Zhuzha");
+
+    double l = 9;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    double a = 4;
+    double b = 5;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
+
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
