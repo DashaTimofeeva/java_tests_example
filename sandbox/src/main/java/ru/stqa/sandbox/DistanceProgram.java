7ce59b972a07f35f4db421eb0def4ca908a90db9
Daria Timofeeva <ddtimofeeva@yandex-team.ru>
2019-10-18 02:01:39 +0300

    Refactoring. Calculation distance between the two points by class method

--- sandbox/src/main/java/ru/stqa/sandbox/DistanceProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/DistanceProgram.java
@@ -4,20 +4,10 @@ public class DistanceProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    p1.x = -4;
-    p1.y = 9;
+    Point p = new Point(-4, 9, 16, 2);
 
-    Point p2 = new Point();
-    p2.x = 16;
-    p2.y = 2;
-
-    System.out.println("Расстояние между точками с координатами: p1 = (" + p1.x + ", " + p1.y + "), " +
-            "p2 = (" + p2.x + ", " + p2.y + "), равно: " + distance(p1, p2));
+    System.out.println("Расстояние между точками с координатами: p1 = (" + p.x1 + ", " + p.y1 + "), " +
+            "p2 = (" + p.x2 + ", " + p.y2 + "), равно: " + p.distance());
 
   }
-  public static double distance(Point p1, Point p2){
-    return Math.sqrt(Math.pow((p2.x - p1.x), 2) + (Math.pow((p2.y - p1.y), 2)));
-  }
-
 }
