353e4a16983eb1f4ebff002b3bc1757012aa15eb
Daria Timofeeva <ddtimofeeva@yandex-team.ru>
2019-10-28 12:18:03 +0300

    Added Constructor for class Point

--- sandbox/src/main/java/ru/stqa/sandbox/DistanceProgram.java
+++ sandbox/src/main/java/ru/stqa/sandbox/DistanceProgram.java
@@ -4,13 +4,8 @@ public class DistanceProgram {
 
   public static void main(String[] args) {
 
-    Point p1 = new Point();
-    p1.x = 1;
-    p1.y = 1;
-
-    Point p2 = new Point();
-    p2.x = 0;
-    p2.y = 0;
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
 
     double res = p1.distance(p2);
     System.out.println("Расстояние между точками с координатами: p1 = (" + p1.x + ", " + p1.y + "), " +
--- sandbox/src/test/java/ru/stqa/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/sandbox/PointTests.java
@@ -7,80 +7,52 @@ public class PointTests {
 
   @Test
   public void testDistanceForOrigin() {
-    Point p1 = new Point();
-    p1.x = 0;
-    p1.y = 0;
-    Point p2 = new Point();
-    p2.x = 0;
-    p2.y = 0;
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(0, 0);
     Assert.assertEquals(p1.distance(p2), 0.0);
   }
 
   @Test
   public void testDistanceForNaturalNumbers() {
-    Point p1 = new Point();
-    p1.x = 10;
-    p1.y = 3;
-    Point p2 = new Point();
-    p2.x = 4;
-    p2.y = 1;
+    Point p1 = new Point(10, 3);
+    Point p2 = new Point(4, 1);
     Assert.assertEquals(p1.distance(p2), 6.324555320336759);
   }
 
   @Test
   public void testDistanceForNegativeNumbers() {
-    Point p1 = new Point();
-    p1.x = -1;
-    p1.y = -1;
-    Point p2 = new Point();
-    p2.x = -4;
-    p2.y = -4;
+    Point p1 = new Point(-1, -1);
+    Point p2 = new Point(-4, -4);
     Assert.assertEquals(p1.distance(p2), 4.242640687119285);
   }
 
   @Test
   public void testDistanceForCombinationNumbers() {
-    Point p1 = new Point();
-    p1.x = -1;
-    p1.y = 0;
-    Point p2 = new Point();
-    p2.x = 4;
-    p2.y = 15;
+    Point p1 = new Point(-1, 0);
+    Point p2 = new Point(4, 15);
     Assert.assertEquals(p1.distance(p2), 15.811388300841896);
   }
 
   @Test
   public void testDistanceForBigNumbers() {
-    Point p1 = new Point();
-    p1.x = 1098;
-    p1.y = 10986;
-    Point p2 = new Point();
-    p2.x = 340;
-    p2.y = 15986;
+    Point p1 = new Point(1098, 10986);
+    Point p2 = new Point(340, 15986);
     Assert.assertEquals(p1.distance(p2), 5057.130016125747);
 
   }
 
   @Test
   public void testDistanceFoFractionalNumbers() {
-    Point p1 = new Point();
-    p1.x = 2.654343;
-    p1.y = 1.000006;
-    Point p2 = new Point();
-    p2.x = 28.076;
-    p2.y = 10.76;
+    Point p1 = new Point(2.654343, 1.000006);
+    Point p2 = new Point(28.076, 10.76);
     Assert.assertEquals(p1.distance(p2), 27.23083045934672);
 
   }
 
   @Test(enabled=false)
   public void testDistanceFailTest() {
-    Point p1 = new Point();
-    p1.x = 1;
-    p1.y = 17;
-    Point p2 = new Point();
-    p2.x = 40;
-    p2.y = 5;
+    Point p1 = new Point(1, 17);
+    Point p2 = new Point(40, 5);
     Assert.assertEquals(p1.distance(p2), 0);
   }
 }
